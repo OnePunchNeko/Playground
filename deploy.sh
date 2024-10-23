6932120531c22ebe0d928f87c604023482735125
KushamiNeko <missing10630@gmail.com>
2024-10-23 13:34:39 +0000

    some cleaning

--- deploy.sh
+++ deploy.sh
@@ -3,25 +3,8 @@
 stackTemplate=$1
 stackName=$2
 resourceGroup=$3
-slackWebhookUrl=$4
-
-# Example Message
-# {
-#   "blocks": [
-#     {
-#       "type": "section",
-#       "text": { "text": "@here Hello Date Picker!!", "type": "mrkdwn" }
-#     }
-#   ]
-# }
-
-# curl -X POST -H 'Content-type: application/json' \
-#     --data "{\"blocks\": [{\"type\": \"section\", \"text\": {\"text\": \"@here ⚠️ Start Deploying Azure Stack *${stackName}* to *${resourceGroup}* ⚠️\", \"type\": \"mrkdwn\"}}]}" \
-#     ${slackWebhookUrl}
 
 # Deploy Azure Stack and keep the exit code without stopping the script 
-exit 1
-
 az stack group create \
     --name ${stackName} \
     --resource-group ${resourceGroup} \
@@ -31,20 +14,9 @@ az stack group create \
     --tags RG=${resourceGroup} \
     --yes || exit_code=$? 
 
+# We could do something here before exit the script
 exit $exit_code
 
-# if [[ -z $exit_code || $exit_code -eq 0 ]]; then
-#     # send success notification
-#     curl -X POST -H 'Content-type: application/json' \
-#         --data "{\"blocks\": [{\"type\": \"section\", \"text\": {\"text\":\"@here ✅ Finish Deploying Azure Stack *${stackName}* to *${resourceGroup}* 🥳\", \"type\": \"mrkdwn\"}}]}" \
-#         ${slackWebhookUrl}
-# else
-#     # send failure notification
-#     curl -X POST -H 'Content-type: application/json' \
-#         --data "{\"blocks\": [{\"type\": \"section\", \"text\": {\"text\":\"@here ❌ Failed Deploying Azure Stack *${stackName}* to *${resourceGroup}* 😨\", \"type\": \"mrkdwn\"}}]}" \
-#         ${slackWebhookUrl}
 
-#     exit 1
-# fi
 
 
